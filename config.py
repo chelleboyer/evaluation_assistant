364a2f433ce40ff958c5719a298846cf620cea02
Michelle Boyer <chelleboyer@comcast.net>
2025-05-13 15:47:33 -0500

    add notebooks & data

--- config.py
+++
@@ -1,7 +0,0 @@
-# OpenAI API configuration
-EMBEDDING_MODEL = "text-embedding-3-small"
-AGENT_MODEL = "gpt-4o-mini"
-RAG_MODEL = "gpt-4o-mini"
-HUGGINGFACE_MODEL = "chelleboyer/llm-evals-2-79b954ef-4798-4994-be72-a88d46b8ecca"
-TAVILY_API_KEY = "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"
-# Add other configuration variables as needed 
